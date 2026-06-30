6d3a24b51e310c723d80f1ab6109e1062ba0a87d
rganiev <rganiev@diasoft.ru>
2026-06-30 14:28:18 +0300

    Коммит от 23.04.2026 Реализация модуля 7

--- src/test/java/ru/otus/service/ProcessQuestionServiceImplTest.java
+++ src/test/java/ru/otus/service/ProcessQuestionServiceImplTest.java
@@ -14,8 +14,8 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@DisplayName("Тесты сервиса ProcessQuestionService")
-class ProcessQuestionServiceTest {
+@DisplayName("Тесты сервиса ProcessQuestionServiceImpl")
+class ProcessQuestionServiceImplTest {
 
     @Mock
     private QuestionsFileService questionsFileService;
